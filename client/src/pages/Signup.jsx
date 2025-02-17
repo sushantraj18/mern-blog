fd17c481b82bf6eb42aa4614e422fcde54c8cd84
sushantraj18 <codexsushant@gmail.com>
2025-02-18 02:04:50 +0530

    signup page functionality

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -1,7 +1,43 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useState } from 'react'
+import toast from 'react-hot-toast'
+import { Link, useNavigate } from 'react-router-dom'
 
 function Signup() {
+  const [formData,setFormData]= useState({})
+  const [loading,setLoading]= useState(false)
+  const navigate = useNavigate()
+
+  const handleChange = (e)=>{
+    setFormData((prev)=>({...prev,[e.target.id] : e.target.value}))
+  }
+
+  
+  const handleSubmit = async (e)=>{
+    e.preventDefault()
+    if(!formData.username || !formData.email || !formData.password){
+      return toast.error('Please fill out all fields')
+    }
+    try{
+      setLoading(true)
+      const res = await fetch('api/auth/register',{
+        method : 'POST',
+        headers : {'Content-Type' : 'application/json'},
+        body : JSON.stringify(formData)
+      })
+      const data = await res.json()
+      setLoading(false)
+      if(data.success === false){
+        return toast.error('Unable to create account')
+      }
+
+      if(res.ok){
+        navigate("/login")
+      }
+    }catch(e){
+      console.error(e)
+    }
+  }
+
   return (
     <div className="flex  items-center p-4 ">
       <div className="text-center  text-white w-[50%] " >
@@ -17,24 +53,24 @@ function Signup() {
               </p>
 
             </div>
-          <form >
+          <form  method='POST' onSubmit={handleSubmit}>
             <div className="w-full flex flex-col gap-2">
               <label className="font-semibold text-xs text-gray-400">Username</label>
-              <input placeholder="Username" id="username" className="border rounded-lg px-3 py-2 mb-5 text-sm w-full outline-none dark:border-gray-500 dark:bg-gray-900" />
+              <input placeholder="Username" id="username" className="border rounded-lg px-3 py-2 mb-5 text-sm w-full outline-none dark:border-gray-500 dark:bg-gray-900" onChange={handleChange} />
             </div>
 
             <div className="w-full flex flex-col gap-2">
               <label className="font-semibold text-xs text-gray-400">Email</label>
-              <input placeholder="yourmail@" id="email" className="border rounded-lg px-3 py-2 mb-5 text-sm w-full outline-none dark:border-gray-500 dark:bg-gray-900" />
+              <input placeholder="yourmail@" id="email" type='email' className="border rounded-lg px-3 py-2 mb-5 text-sm w-full outline-none dark:border-gray-500 dark:bg-gray-900" onChange={handleChange} />
             </div>
 
             <div className="w-full flex flex-col gap-2">
             <label className="font-semibold text-xs text-gray-400">Password</label>
-            <input placeholder="••••••••" id="password" className="border rounded-lg px-3 py-2 mb-5 text-sm w-full outline-none dark:border-gray-500 dark:bg-gray-900" type="password" />
+            <input placeholder="••••••••" id="password" type='password' className="border rounded-lg px-3 py-2 mb-5 text-sm w-full outline-none dark:border-gray-500 dark:bg-gray-900"  onChange={handleChange} />
           </div>
           <div>
-            <button className="py-1 px-8 bg-blue-500 hover:bg-blue-800 focus:ring-offset-blue-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2 rounded-lg cursor-pointer select-none">
-              Sign up
+            <button disabled={loading} className="py-1 px-8 bg-blue-500 hover:bg-blue-800 focus:ring-offset-blue-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2 rounded-lg cursor-pointer select-none">
+              {loading ? 'Loading...' :'Sign Up'}
             </button>
           </div>
           </form>
--- client/vite.config.js
+++ client/vite.config.js
@@ -1,6 +1,17 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
+  server : {
+
+    proxy:{
+      '/api':{
+        target : 'http://localhost:3000',
+        secure : false,
+        
+      }
+    }
+
+  },
   plugins: [
     tailwindcss(),
   ],
