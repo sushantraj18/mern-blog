df52ce685163d0c199bb2a145089424e3c3d883f
sushantraj18 <codexsushant@gmail.com>
2025-02-22 13:12:01 +0530

    create login api and little correction in error middleware

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,6 +1,7 @@
 const bcrypt = require("bcrypt")
 const userModel = require("../models/userModel")
 const error = require("../middleware/error")
+const jwt = require('jsonwebtoken')
 
 const registerUser = async (req,res,next)=>{
     const {username,email,password} = req.body
@@ -9,16 +10,25 @@ const registerUser = async (req,res,next)=>{
         return next(error(400,"All fields are required"))
     }
 
-    const hasPassword = bcrypt.hashSync(password,12)
-
-    const newUser = new userModel({
-        username,
-        email,
-        password:hasPassword,
-    })
-
+    
+    
     try{
+        
+        const isUserAlreadyExists  = await userModel.findOne({$or : [{username},{email}]})
+        
+        if(isUserAlreadyExists){
+            return next(error(409,"user already exists"))
+        }
+        const hasPassword = bcrypt.hashSync(password,12)
+        
 
+
+        const newUser = new userModel({
+            username,
+            email,
+            password:hasPassword,
+        })
+    
         await newUser.save()
         res.status(201).json({success : true , message : "User created successfully"})
 
@@ -27,4 +37,41 @@ const registerUser = async (req,res,next)=>{
     }
 }
 
-module.exports = {registerUser}
+
+const loginUser = async(req,res,next)=>{
+
+    const {email,password} = req.body
+
+    if(!email || !password || email === '' || password === ''){
+        return next(error(400,"All fields are required"))
+    }
+
+
+
+    try{
+
+        const isUserAvailable = await userModel.findOne({email})
+
+        if(!isUserAvailable){
+            return next(error(400,"invalid user or password"))
+        }
+
+        const checkPass = bcrypt.compareSync(password,isUserAvailable.password)
+
+        if(!checkPass){
+            return next(error(400,"invalid password or user"))
+        }
+
+
+        const accessToken = jwt.sign({user : isUserAvailable.username},process.env.JWT_TOKEN_KEY,{expiresIn:"1d"})
+
+        res.status(200).cookie('access-token',accessToken,{httpOnly:true}).json("login successfully")
+
+    }catch(e){
+        console.log(e)
+        next(e)
+    }
+}
+
+
+module.exports = {registerUser,loginUser}
--- server/middleware/error.js
+++ server/middleware/error.js
@@ -1,6 +1,6 @@
 const error = (statusCode,message)=>{
-    const error = new Error
-    error.status = statusCode;
+    const error = new Error()
+    error.statusCode = statusCode;
     error.message = message
     return error
 }
